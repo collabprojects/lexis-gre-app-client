20bca98b9c5f836cc06876722987e4b9b287345d
DipeshMohanty1098 <dipeshmohanty0226@gmail.com>
2021-07-10 01:13:39 +0530

    initial commit

---
+++ src/components/words.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Loading from '../shared/loading'
+
+
+class Words extends React.Component{
+    state = {
+        words: null
+    }
+
+    componentDidMount() {
+        fetch('http://127.0.0.1:5000/words/').then((res) => {
+            if (res.ok){
+                return res.json()
+            } 
+        }).then((data) => {
+            this.setState({
+                words: data
+            })
+        })
+    }
+
+    render(){
+        const state = this.state
+        const wordList = state.words ? <div className="word_table">
+            <table className="centered">
+            <tr>
+            <th>Word</th>
+            <th>Meaning</th>
+            <th>Second</th>
+            <th>Sentence</th>
+            </tr>
+            {state.words.map((word) => {
+                return (
+                    <tr key={word.id}>
+                    <td>{word.word}</td>
+                    <td>{word.primary_meaning}</td>
+                    <td>{word.secondary_meaning}</td>
+                    <td>{word.sentence}</td>
+                    </tr>
+                )
+            })}
+            </table>
+        </div> : <Loading/>
+        return (
+            <div className = "words">
+                {wordList}
+            </div>
+        )
+    }
+
+}
+
+export default Words
