9ba11a45ae9f35c4b5da0d60982ba3e8fd62f940
DipeshMohanty1098 <dipeshmohanty0226@gmail.com>
2021-07-24 01:03:38 +0530

    Added Textarea to for big inputs

--- src/components/word_form_update.js
+++ src/components/word_form_update.js
@@ -107,9 +107,9 @@ class WordFormUpdate extends React.Component {
                     <h5><b><u>Update Word</u></b></h5>
                     <form className="center">
                         <input type='text' id='word' value={this.state.word} placeholder='Enter word' onChange={this.handleChange} />
-                        <input type='text' id='primary_meaning' value={this.state.primary_meaning} placeholder='Enter Primary Meaning' onChange={this.handleChange} />
+                        <textarea class="materialize-textarea" type='text' id='primary_meaning' value={this.state.primary_meaning} placeholder='Enter Primary Meaning' onChange={this.handleChange} />
                         <input type='text' id='secondary_meaning' value={this.state.secondary_meaning} placeholder='Enter Secondary Meaning' onChange={this.handleChange} />
-                        <input type='text' id='sentence' value={this.state.sentence} placeholder='Enter Sentence' onChange={this.handleChange} />
+                        <textarea class="materialize-textarea" type='text' id='sentence' value={this.state.sentence} placeholder='Enter Sentence' onChange={this.handleChange} />
                         <input type='text' id='synonym' value={this.state.synonym} placeholder='Enter Synonyms' onChange={this.handleChange} />
                         <input type='text' id='antonym' value={this.state.antonym} placeholder='Enter Antonyms' onChange={this.handleChange} />
                     </form>
